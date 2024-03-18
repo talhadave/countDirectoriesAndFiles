d55fbaf99d9d770dbd086ae9663442d093009b2d
talha iqbal <talhadev13@gmail.com>
2024-03-18 19:15:42 +0500

    adding debug

--- Task1.py
+++ Task1.py
@@ -1,7 +1,8 @@
 import os
 def countDirectoryAndFiles(path):
     
-     files =directories = 0
+    print("hello world") 
+    files =directories = 0
 
      for _, dirnames, filenames in os.walk(path):
          files += len(filenames)
