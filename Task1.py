f367ab4445b849d9bc4c31b2dc27cf9590cc6f62
Talha Iqbal <talhadev13@gmail.com>
2024-04-17 20:10:24 +0500

    add Task1.py

---
+++ Task1.py
@@ -0,0 +1,16 @@
+import os
+def countDirectoryAndFiles(path):
+
+    files =directories = 0
+
+    for _, dirnames, filenames in os.walk(path):
+         files += len(filenames)
+         directories += len(dirnames)
+
+
+    return (files,directories)
+     
+    
+
+path=os.getcwd()
+print(countDirectoryAndFiles(path))
